5e209b87cbc9bbb65e9c3aec48a15e0c8315850f
Mudit Ameta <zeusdeux@gmail.com>
2014-12-17 01:47:03 +0530

    [0.3.3] Added no results transform and it's test

---
+++ src/transforms/noResults.js
@@ -0,0 +1,23 @@
+var util     = require('../util');
+var cu       = require('auto-curry');
+var messages = require('../messages');
+var isArray  = util.isArray;
+var isObject = util.isObject;
+
+
+function noResults(msg) {
+  return function(dataList) {
+    if (isObject(dataList)) {
+      if (isArray(dataList)) {
+        if (!dataList.length) dataList.push(msg || 'No Results found.');
+      }
+      else {
+        if (isArray(dataList.data) && !dataList.data.length) dataList.data.push(msg || 'No Results found.');
+      }
+      return dataList;
+    }
+    else throw new SyntaxError(messages.DataMustBeArrayOrObject);
+  };
+}
+
+module.exports = cu(noResults);
---
+++ test/transforms.noResults.test.js
@@ -0,0 +1,38 @@
+var search    = require('../src').search;
+var noResults = require('../src/transforms/noResults');
+
+
+describe('transforms#noResults', function() {
+  describe('when a search doesn\'t match anything', function() {
+    it('should output should be given string to noResults', function() {
+      var data = ['omg man', 'this is a test', 'woah that was some crazy shizzle', 'merry chrismizzle bizzle'];
+      var data2 = {
+        data: [{
+          val: 1,
+          text: 'lets give this a shot'
+        }, {
+          val: 2,
+          text: 'omg potatoes'
+        }, {
+          val: 3,
+          text: 'i have no idea what i am typing'
+        }, {
+          val: 4,
+          text: 'yay for long island ice teas'
+        }],
+        searchInProps: ['text']
+      };
+      var temp;
+
+      temp = search({
+        noResults: noResults('No results found.')
+      }, data, 'boopity');
+      temp.should.be.eql(['No results found.']);
+
+      temp = search({
+        noResults: noResults('No results found son.')
+      }, data2, 'zebra');
+      temp.data.should.be.eql(['No results found son.']);
+    });
+  });
+});
